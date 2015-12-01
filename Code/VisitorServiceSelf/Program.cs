ba0590a902b2f5aab2d1dad5af08dddf00d2d938
netcoder <792135648@qq.com>
2015-12-01 17:59:42 +0800

    ok

--- Code/VisitorServiceSelf/Program.cs
+++ Code/VisitorServiceSelf/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,7 +19,61 @@ namespace VisitorServiceSelf
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmMainWindow());
+            Process instance = RunningInstance();
+            if (null == instance)
+            {
+                Application.Run(new FrmMainWindow());
+            }
+            else
+            {
+                HandleRunningInstance(instance);
+            }
+        }
+
+        private static void HandleRunningInstance(Process instance)
+        {
+            IntPtr winPtr = IntPtr.Zero;
+            if (instance.MainWindowHandle == IntPtr.Zero)
+            {
+                winPtr = FindWindow(null, "闸机控制软件");
+            }
+            else
+            {
+                winPtr = instance.MainWindowHandle;
+            }
+            ShowWindowAsync(winPtr, 1);  //调用api函数，正常显示窗口
+            SetForegroundWindow(winPtr); //将窗口放置最前端
+        }
+
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow")]
+        private extern static IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("User32.dll")]
+        private static extern bool SetForegroundWindow(System.IntPtr hWnd);
+
+        [DllImport("User32.dll")]
+        private static extern bool ShowWindowAsync(System.IntPtr hWnd, int cmdShow);
+
+
+        private static Process RunningInstance()
+        {
+            Process current = Process.GetCurrentProcess();
+            Process[] processes = Process.GetProcessesByName(current.ProcessName);
+            //遍历与当前进程名称相同的进程列表  
+            foreach (Process process in processes)
+            {
+                //如果实例已经存在则忽略当前进程  
+                if (process.Id != current.Id)
+                {
+                    //保证要打开的进程同已经存在的进程来自同一文件路径
+                    if (Assembly.GetExecutingAssembly().Location.Replace("/", "//") == current.MainModule.FileName)
+                    {
+                        //返回已经存在的进程
+                        return process;
+                    }
+                }
+            }
+            return null;
         }
     }
 }
