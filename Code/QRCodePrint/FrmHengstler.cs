f202821a01fc31acb49652478935f57b1b4fc102
netcoder <792135648@qq.com>
2015-12-17 16:05:22 +0800

    ok

--- Code/QRCodePrint/FrmHengstler.cs
+++ Code/QRCodePrint/FrmHengstler.cs
@@ -17,7 +17,8 @@ namespace QRCodePrint
         const string init = "1B 40"; //软件初始化
         const string fontbold = "1B 45 00"; //粗体
         const string underline = "1B 2D 00"; //下划线
-        const string ht = "1B 61 01"; //水平对齐  0 left 1 center 2 right
+        const string ht_l = "1B 61 00";
+        const string ht_c = "1B 61 01"; //水平对齐  0 left 1 center 2 right
         const string characterSize = "1B 21 12"; //字体大小
         const string line1 = "1B 64 01"; //打印并前进N行
         const string line2 = "1B 64 02"; //打印并前进N行
@@ -60,12 +61,28 @@ namespace QRCodePrint
             data.AddRange(init.ToByte());
             data.AddRange(fontbold.ToByte());
             data.AddRange(underline.ToByte());
-            data.AddRange(ht.ToByte());
+            data.AddRange(ht_c.ToByte());
             data.AddRange(characterSize.ToByte());
             data.AddRange(line1.ToByte());
 
             data.AddRange("小票标题".ToGb2312());
             data.AddRange(line4.ToByte());
+
+            data.AddRange("入场：2012-08-31 10:53".ToGb2312());
+            data.AddRange(line1.ToByte());
+
+            data.AddRange("通道：一号入口".ToGb2312());
+            data.AddRange(line1.ToByte());
+
+            data.AddRange("厂商：深圳来吉智能科技".ToGb2312());
+            data.AddRange(line1.ToByte());
+
+            data.AddRange("先到收费处交费，再取车出场".ToGb2312());
+            data.AddRange(line1.ToByte());
+
+            data.AddRange(new byte[] { 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D });
+            data.AddRange(line1.ToByte());
+
             data.AddRange(cutpaper.ToByte());
 
             printer.Write(data.ToArray());
@@ -77,7 +94,7 @@ namespace QRCodePrint
             data.AddRange(init.ToByte());
             data.AddRange(fontbold.ToByte());
             data.AddRange(underline.ToByte());
-            data.AddRange(ht.ToByte());
+            data.AddRange(ht_c.ToByte());
             data.AddRange(characterSize.ToByte());
             data.AddRange(line1.ToByte());
 
@@ -92,6 +109,22 @@ namespace QRCodePrint
                 data.Add((byte)c);
             }
             data.AddRange(line2.ToByte());
+
+            data.AddRange("入场：2012-08-31 10:53".ToGb2312());
+            data.AddRange(line1.ToByte());
+
+            data.AddRange("通道：一号入口".ToGb2312());
+            data.AddRange(line1.ToByte());
+
+            data.AddRange("厂商：深圳来吉智能科技".ToGb2312());
+            data.AddRange(line1.ToByte());
+
+            data.AddRange("先到收费处交费，再取车出场".ToGb2312());
+            data.AddRange(line1.ToByte());
+
+            data.AddRange(new byte[] { 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D });
+            data.AddRange(line1.ToByte());
+
             data.AddRange(cutpaper.ToByte());
 
             printer.Write(data.ToArray());
@@ -103,9 +136,12 @@ namespace QRCodePrint
             data.AddRange(init.ToByte());
             data.AddRange(fontbold.ToByte());
             data.AddRange(underline.ToByte());
-            data.AddRange(ht.ToByte());
+            data.AddRange(ht_c.ToByte());
             data.AddRange(characterSize.ToByte());
-            data.AddRange(line1.ToByte());
+            data.AddRange(line2.ToByte());
+
+            data.AddRange("欢迎到访帝王大厦".ToByte());
+            data.AddRange(line2.ToByte());
 
             data.AddRange(barcodeH.ToByte());
             data.AddRange(barcodesetting2.ToByte());
@@ -116,6 +152,17 @@ namespace QRCodePrint
                 data.Add((byte)c);
             }
             data.AddRange(line2.ToByte());
+
+            //文字内容 
+            //左对齐
+            data.AddRange(ht_l.ToByte());
+            data.AddRange("访客：张三".ToGb2312());
+            data.AddRange(line1.ToByte());
+            data.AddRange("时间：2015-12-12 09:23:45".ToGb2312());
+            data.AddRange(line1.ToByte());
+            data.AddRange(line1.ToByte());
+            data.AddRange(line1.ToByte());
+
             data.AddRange(cutpaper.ToByte());
 
             printer.Write(data.ToArray());
