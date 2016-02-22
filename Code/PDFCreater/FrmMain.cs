27484ff60afbba799954bcf55a2c67be244560c8
netcoder <792135648@qq.com>
2016-02-22 16:00:05 +0800

    pdf

---
+++ Code/PDFCreater/FrmMain.cs
@@ -0,0 +1,20 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PDFCreater
+{
+    public partial class FrmMain : FrmBase
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
