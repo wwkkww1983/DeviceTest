dd182978c924dcfb217660b3fe36f145670f9307
ysj <ysjr-2002@163.com>
2016-04-27 11:05:44 +0800

    usb

---
+++ Code/DeskoUsbQRReader/FrmVComReader.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Common;
+using System.IO.Ports;
+using System.Diagnostics;
+using System.Threading;
+namespace DeskoUsbQRReader
+{
+    /// <summary>
+    /// Desko Usb虚拟串口二维码阅读器
+    /// 1.安装驱动
+    /// 2.安装虚拟串口
+    /// </summary>
+    public partial class FrmVComReader : FrmBase
+    {
+
+        public FrmVComReader()
+        {
+            InitializeComponent();
+        }
+
+        private void log(string str)
+        {
+            Debug.WriteLine(str);
+        }
+
+        SerialPort _serial = null;
+        bool _isStop = false;
+        static object expectResponseMutex = new object();
+        private void FrmVComReader_Load(object sender, EventArgs e)
+        {
+            _serial = new SerialPort("Com34", 19200, Parity.None, 8, StopBits.One);
+            _serial.Open();
+            _serial.RtsEnable = true;
+            _serial.ReceivedBytesThreshold = 1;
+            _serial.DataReceived += _serial_DataReceived;
+            log("port open");
+            //new Thread(Read).Start();
+
+            _serial.Write("DESKO.CONTROL.ENABLE=1");
+            lock (expectResponseMutex)
+            {
+                if (Monitor.Wait(expectResponseMutex, 1000))
+                {
+
+                }
+            }
+        }
+
+        void _serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            lock (expectResponseMutex)
+            {
+                string str = _serial.ReadExisting();
+
+                var num = str.IndexOf('\u0002');
+                var num2 = str.IndexOf('\u0003');
+                var num3 = str.IndexOf('\r');
+                log(num + " " + num2 + " " + num3);
+
+                if (num != -1)
+                    str = str.Substring(num + 2, (num3 - num - 2));
+                log(str);
+            }
+        }
+
+        private void Read()
+        {
+            //while (!_isStop)
+            //{
+            //    try
+            //    {
+            //        var pos = 0;
+            //        byte[] buffer = new byte[128];
+            //        var stx = (byte)_serial.ReadByte();
+            //        if (stx != 0x02)
+            //            continue;
+
+            //        buffer[pos] = stx;
+            //        pos++;
+            //        byte b = 0;
+            //        while ((b = (byte)_serial.ReadByte()) > 0)
+            //        {
+            //            buffer[pos] = b;
+            //            pos++;
+            //            if (b == 0x03)
+            //                break;
+            //        }
+            //        PrintCode(buffer);
+            //    }
+            //    catch
+            //    {
+            //        Console.WriteLine("串口关闭");
+            //    }
+            //}
+        }
+
+        private void PrintCode(byte[] buffer)
+        {
+            var sb = new StringBuilder();
+            for (var i = 0; i < buffer.Length; i++)
+            {
+                sb.Append(buffer[i] + " ");
+            }
+            Invoke(new Action(() =>
+            {
+                var codeStr = Encoding.UTF8.GetString(buffer, 2, buffer.Length - 3);
+                log(DateTime.Now + " " + sb.ToString());
+                log(DateTime.Now + " " + codeStr);
+            }));
+        }
+    }
+}
---
+++ Code/DeskoUsbQRReader/Scanner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeskoUsbQRReader
+{
+    public static class Scanner
+    {
+        private const byte STX = 0x02;
+        private const byte ETX = 0x03;
+    }
+}
