4822ab39ac3f59242f03d6edb8d1b50794d2ab7f
ysj <ysjr-2002@163.com>
2017-04-28 18:43:17 +0800

    ok

--- Code/YowhaleAPI/FrmGateMain.cs
+++ Code/YowhaleAPI/FrmGateMain.cs
@@ -6,12 +6,14 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Script.Serialization;
 using System.Windows.Forms;
 
 namespace MeetingClient
@@ -64,6 +66,14 @@ namespace MeetingClient
                         var code = "https://visitor.shenjing.com/openapi/guard/openDoor.json?app=guard&content=visitor%3A%2F%2Farrive%3Fcode%3Ddf18392b-9b7e-4d73-a516-7d56fe67d6b8&guard=2&sign=46aab8e44e151fbc3ebf2b94a09c0c01&sign_type=MD5";
                         var flag = requestAPI.VerfiyAccess(code);
                         sw.Stop();
+                        //var request = WebRequest.Create(code);
+                        //var response = request.GetResponse();
+                        //var streamReader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                        //var content = streamReader.ReadToEnd();
+                        //streamReader.Close();
+                        //JavaScriptSerializer java = new JavaScriptSerializer();
+                        //var jsonObject = java.Deserialize<ErrorModel>(content);
+                        //var flag = jsonObject.content;
                         Debug.WriteLine("open door:" + flag + " " + sw.ElapsedMilliseconds);
                     }
                     Thread.Sleep(1000);
